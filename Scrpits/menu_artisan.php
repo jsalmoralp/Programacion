3fa5b061f574893ea3139d20d0ea3b812519851a
Joan Salmoral Parramón <jsalmoralp@gmail.com>
2024-12-21 13:21:57 +0100

    Menú Artisan -V2-
    Mejora al Menú para manejar los Comandos de Artisan.

--- Scrpits/menu_artisan.php
+++ Scrpits/menu_artisan.php
@@ -6,15 +6,50 @@ $secciones = [
         "1" => "migrate",
         "2" => "migrate:rollback",
         "3" => "migrate:refresh",
-        "4" => "db:seed",
+        "4" => "migrate:reset",
+        "5" => "migrate:status",
+        "6" => "db:seed",
+        "7" => "db:wipe",
+    ],
+    "Cache y cola" => [
+        "1" => "cache:clear",
+        "2" => "cache:forget",
+        "3" => "queue:work",
+        "4" => "queue:listen",
+        "5" => "queue:restart",
+        "6" => "queue:failed",
     ],
     "Modelos y migraciones" => [
         "1" => "make:model",
         "2" => "make:migration",
+        "3" => "make:factory",
+        "4" => "make:seeder",
     ],
     "Controladores" => [
         "1" => "make:controller",
         "2" => "make:resource",
+        "3" => "make:request",
+        "4" => "make:policy",
+    ],
+    "Vistas" => [
+        "1" => "make:view",
+        "2" => "view:clear",
+    ],
+    "Rutas" => [
+        "1" => "route:list",
+        "2" => "route:cache",
+        "3" => "route:clear",
+    ],
+    "Tests" => [
+        "1" => "test",
+        "2" => "make:test",
+    ],
+    "Miscelánea" => [
+        "1" => "key:generate",
+        "2" => "storage:link",
+        "3" => "vendor:publish",
+        "4" => "inspire",
+        "5" => "tinker",
     ],
     // ... más secciones y comandos
 ];
@@ -42,16 +77,20 @@ while (true) {
     }
 
     // Procesa la opción seleccionada
+    $comandoEncontrado = false;
     foreach ($secciones as $comandos) {
         if (isset($comandos[$opcion])) {
             $comando = $comandos[$opcion];
             echo "Ejecutando comando: php artisan " . $comando . "\n";
             system("php artisan " . $comando);
-            break 2; // Sale de los dos bucles
+            $comandoEncontrado = true;
+            break;
         }
     }
 
-    echo "Opción inválida. Inténtalo de nuevo.\n";
+    if (!$comandoEncontrado) {
+        echo "Opción inválida. Inténtalo de nuevo.\n";
+    }
 }
 
 echo "¡Hasta luego!\n";
